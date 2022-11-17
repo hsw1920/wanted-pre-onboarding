c503bd217bfcc6db775695350de9a5d278dd1772
hsw1920 <hsw1920@gmail.com>
2022-11-17 16:32:35 +0900

    feat: 성적추가(변경)기능 추가

--- MyCreditManager/MyCreditManager/main.swift
+++ MyCreditManager/MyCreditManager/main.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 
+
+// 학생 class
 class Students {
     var name: String
     var subjects: [String:String] = [:]
@@ -15,7 +17,7 @@ class Students {
     }
 }
 
-
+// 학생 class 리스트
 var studentList: [Students] = []
 var quit : Bool = false
 
@@ -80,6 +82,73 @@ func deleteStudent(){
     
 }
 
+// MARK: - 성적추가
+func addSubject(){
+    print("성적을 추가할 학생의 이름, 과목 이름, 성적(A+, A, F 등)을 띄어쓰길 구분하여 차례로 작성해주세요.")
+    print("입력예) Mickey Swift A+")
+    print("만약에 학생의 성적 중 해당 과목이 존재하면 기존 점수가 갱신됩니다.")
+    
+    let input = readLine()!
+    
+    let studentName: String
+    let subject: String
+    let subjectScore: String
+    
+    // 먼저 input으로 들어온 문자열을 공백을 기준으로 나누어서 name subject score로 저장함
+    let splitInput = input.split(separator: " ")
+    
+    // 잘못입력하는 경우?
+    // 1. 3가지가 아닌경우
+    // 2. 학생이 이름이 없는경우
+    
+    // 공백기준 3가지 입력이 아닌경우
+    if splitInput.count != 3 {
+        print("입력이 잘못되었습니다. 다시 확인해주세요.")
+        return
+    } else {
+        studentName = String(splitInput[0])
+        subject = String(splitInput[1])
+        subjectScore = String(splitInput[2])
+    }
+    
+    // 학생이름 존재하는지 확인
+    let hasStudent = studentList.contains { student -> Bool in
+        if student.name == studentName {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    // 학생이름 없으면 리턴
+    if !hasStudent {
+        print("\(studentName) 학생을 찾지 못했습니다.")
+        return
+    }
+    
+    // 성적이 잘못 입력되면 리턴
+    let Scores = ["A+","A","B+","B","C+","C","D+","D","F"]
+    let correctScore = Scores.contains(subjectScore)
+    if !correctScore {
+        print("성적 입력이 올바르지 않습니다.")
+        return
+    }
+    
+    
+    
+    for student in studentList {
+        if student.name == studentName {
+            student.subjects[subject] = subjectScore
+            print("\(studentName) 학생의 \(subject) 과목이 \(subjectScore)로 추가(변경)되었습니다.")
+            print(student.subjects)
+            return
+        }
+    }
+
+    
+}
+
+
 func startFunc(_ cases: String){
     
     switch cases{
@@ -88,7 +157,7 @@ func startFunc(_ cases: String){
     case "2" : // 학생 삭제
         deleteStudent()
     case "3" : // 성적추가(변경)
-        print("3")
+        addSubject()
     case "4" : // 성적삭제
         print("4")
     case "5" : // 평점보기
